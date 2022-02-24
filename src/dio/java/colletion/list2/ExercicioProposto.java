b338a7cea943a760f067cf94bd54dc72d1d33b25
alenitycruz <alessandradsmcruz@recife.pe.senac.br>
2022-02-23 23:48:42 -0300

    Exercicios de List do Collections Java

---
+++ src/dio/java/colletion/list2/ExercicioProposto.java
@@ -0,0 +1,10 @@
+package dio.java.colletion.list2;
+
+/*
+Faça um programa que receba a temperatura média dos 6 primeiros meses do ano e armazena-as em uma lista.
+Após isto, calcule a média semestral das temperaturas e mostre todas as temperaturas acima desta média,
+e em que mês elas ocorreram (mostras o mês por extenso: 1 - janeiro, 2 - fevereiro etc...)
+ */
+
+public class ExercicioProposto {
+}
